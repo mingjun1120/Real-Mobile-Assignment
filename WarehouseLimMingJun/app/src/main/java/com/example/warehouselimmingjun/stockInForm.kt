cf76f71dfc981e07f5af37e8a49ce532330dc21e
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2020-11-19 01:36:26 +0800

    Update stockInForm.kt
    Add validation

--- WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/stockInForm.kt
+++ WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/stockInForm.kt
@@ -2,6 +2,7 @@ package com.example.warehouselimmingjun
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -19,14 +20,40 @@ class stockInForm : AppCompatActivity() {
 
         val confirmBtn = findViewById<ImageButton>(R.id.confirmButton)
         confirmBtn.setOnClickListener{
-            Toast.makeText(this,"Increased!", Toast.LENGTH_SHORT).show()
+
             val intent = Intent(this, HomeScreen::class.java)
-            startActivity(intent)
+
+            val myQty = findViewById<EditText>(R.id.QuantityText)
+            val checkQty = validateQtyField(myQty)
+
+            if (checkQty)
+            {
+                Toast.makeText(this,"Stock added successfully!", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+            }
         }
 
         val clearBtn = findViewById<ImageButton>(R.id.clearButton)
         clearBtn.setOnClickListener{
 
         }
+    }
+
+    private fun validateQtyField(myQty: EditText): Boolean {
+
+        if (myQty.text.toString().isEmpty()) {
+            myQty.error = "Field can't be empty!"
+        }
+        else if (myQty.text.toString().contains(" ")) {
+            myQty.error = "No whitespace!"
+        }
+        else if (!(myQty.text.toString().matches("-?\\d+(\\.\\d+)?".toRegex()))) {
+            myQty.error = "Only numeric characters!"
+        }
+        else {
+            myQty.error = null
+            return true
+        }
+        return false
     }
 }
