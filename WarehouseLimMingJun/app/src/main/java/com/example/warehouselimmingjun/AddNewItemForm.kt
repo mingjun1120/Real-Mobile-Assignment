269e866b0581c95ef486e60aa6d7b4c74c791b84
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2020-11-19 18:51:11 +0800

    Update AddNewItemForm.kt
    Do alignment

--- WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/AddNewItemForm.kt
+++ WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/AddNewItemForm.kt
@@ -2,6 +2,7 @@ package com.example.warehouselimmingjun
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -21,12 +22,105 @@ class AddNewItemForm : AppCompatActivity() {
         confirmBtn.setOnClickListener{
             Toast.makeText(this,"Added!", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, HomeScreen::class.java)
-            startActivity(intent)
+
+            val myProductID = findViewById<EditText>(R.id.ProductIDText)
+            val checkProductID = validateProductID(myProductID)
+
+            val myProductName = findViewById<EditText>(R.id.ProductNameText)
+            val checkProductName = validateProductName(myProductName)
+
+            val myProductQty = findViewById<EditText>(R.id.QuantityText)
+            val checkProductQty = validateProductQty(myProductQty)
+
+            val myProductPrice = findViewById<EditText>(R.id.sizeText)
+            val checkProductPrice = validateProductPrice(myProductPrice)
+
+            if(checkProductID && checkProductName && checkProductQty && checkProductPrice)
+            {
+                startActivity(intent)
+            }
         }
 
         val clearBtn = findViewById<ImageButton>(R.id.clearButton)
         clearBtn.setOnClickListener{
 
         }
+    }
+
+    private fun validateProductPrice(myProductPrice: EditText): Boolean {
+
+        if (myProductPrice.text.toString().isEmpty()) {
+            myProductPrice.error = "Field can't be empty!"
+        }
+        else if (myProductPrice.text.toString().contains(" ")) {
+            myProductPrice.error = "No whitespace!"
+        }
+        else if ((myProductPrice.text.toString().toDoubleOrNull()) == null) {
+            myProductPrice.error = "Only decimal numbers!"
+        }
+        else {
+            myProductPrice.error = null
+            return true
+        }
+        return false
+    }
+
+    private fun validateProductName(myProductName: EditText): Boolean {
+
+        val myPattern: Regex = Regex("^[A-Z][A-Za-z()* ]*\$")
+
+        if (myProductName.text.toString().isEmpty()) {
+            myProductName.error = "Field can't be empty!"
+        }
+        else if (myProductName.text.toString().first() == myProductName.text.toString().capitalize().first()) {
+            myProductName.error = "1st character must be uppercase!"
+        }
+        else if (!(myPattern.matches(myProductName.text.toString()))) {
+            myProductName.error = "No special characters! Eg:!@#"
+        }
+        else {
+            myProductName.error = null
+            return true
+        }
+        return false
+    }
+
+    private fun validateProductID(myProductID: EditText): Boolean {
+
+        val myPattern: Regex = Regex("^[A-Z][A-Z0-9*]*\$")  //SD004M
+
+        if (myProductID.text.toString().isEmpty()) {
+            myProductID.error = "Field can't be empty!"
+        }
+        else if (myProductID.text.toString().contains(" ")) {
+            myProductID.error = "No whitespace!"
+        }
+        else if (!(myPattern.matches(myProductID.text.toString()))) {
+            myProductID.error = "Only uppercase & numeric characters!"
+        }
+        else {
+            myProductID.error = null
+            return true
+        }
+        return false
+    }
+
+
+    private fun validateProductQty(myProductQty: EditText): Boolean {
+
+        if (myProductQty.text.toString().isEmpty()) {
+            myProductQty.error = "Field can't be empty!"
+        }
+        else if (myProductQty.text.toString().contains(" ")) {
+            myProductQty.error = "No whitespace!"
+        }
+        else if (!(myProductQty.text.toString().matches("-?\\d+(\\.\\d+)?".toRegex()))) {
+            myProductQty.error = "Only numeric characters!"
+        }
+        else {
+            myProductQty.error = null
+            return true
+        }
+        return false
     }
 }
