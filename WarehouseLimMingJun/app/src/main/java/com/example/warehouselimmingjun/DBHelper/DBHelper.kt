157284feddfff30362836de2d77851a32023bcbd
yanzhi1998 <44216291+yanzhi1998@users.noreply.github.com>
2020-11-20 17:50:15 +0800

    validate Email
    Email register cannot be exists

--- WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/DBHelper/DBHelper.kt
+++ WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/DBHelper/DBHelper.kt
@@ -4,11 +4,10 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.provider.ContactsContract
-import com.example.warehouselimmingjun.Login
+import android.util.Log
 import com.example.warehouselimmingjun.model.Register
 
-class DBHelper(context : Context):SQLiteOpenHelper(context,DATABASE_NAME, null,DATABASE_VER) {
+class DBHelper(context: Context):SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VER) {
     companion object {
             private val DATABASE_VER = 1
             private val DATABASE_NAME = "WAREHOUSE.db"
@@ -37,12 +36,12 @@ class DBHelper(context : Context):SQLiteOpenHelper(context,DATABASE_NAME, null,D
         values.put(COL_PASS, register.pass)
         values.put(COL_NAME, register.name)
 
-        db.insert(TABLE_NAME, null,values)
+        db.insert(TABLE_NAME, null, values)
         db.close()
 
     }
 
-    fun Login(email:String, pass:String):Boolean
+    fun Login(email: String, pass: String):Boolean
     {
         val db = readableDatabase
         val query = "select * from $TABLE_NAME where $COL_EMAIL = '$email' and $COL_PASS = '$pass'"
@@ -55,6 +54,21 @@ class DBHelper(context : Context):SQLiteOpenHelper(context,DATABASE_NAME, null,D
         }
         cursor.close()
         return true
+    }
+
+    fun checkEmailExist(email: String): Boolean
+    {
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME WHERE $COL_EMAIL ='$email'"
+        val cursor = db.rawQuery(query, null)
+
+        if (cursor.count>0)
+        {
+            cursor.close()
+            return true
+        }
+        cursor.close()
+        return false
     }
 
 }
