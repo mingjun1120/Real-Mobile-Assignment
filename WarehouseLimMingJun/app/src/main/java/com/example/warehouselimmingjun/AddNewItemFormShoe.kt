275e738c490984866baf972603f2ad59775f6496
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2020-11-21 15:14:46 +0800

    Update AddNewItemFormShoe.kt
    Change context

--- WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/AddNewItemFormShoe.kt
+++ WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/AddNewItemFormShoe.kt
@@ -184,7 +184,7 @@ class AddNewItemFormShoe : AppCompatActivity(), AdapterView.OnItemSelectedListen
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         val text = parent!!.getItemAtPosition(position).toString()
-        Toast.makeText(this@AddNewItemFormShoe, text, Toast.LENGTH_SHORT).show()
+        Toast.makeText(parent.context, text, Toast.LENGTH_SHORT).show()
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
