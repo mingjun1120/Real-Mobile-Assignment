5ac77a0be93bd294c4bb40ff18596cb83dba8003
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2020-11-29 06:11:30 +0800

    Update ItemOutAdapter.kt
    Edit ItemOutAdapter.kt for StockOut.kt
    
    Pass value to stockOutForm

--- WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/adapter/ItemOutAdapter.kt
+++ WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/adapter/ItemOutAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.warehouselimmingjun.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,25 +11,29 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.warehouselimmingjun.R
+import com.example.warehouselimmingjun.model.Item
 import com.example.warehouselimmingjun.model.ItemList
+import com.example.warehouselimmingjun.stockOutForm
 
 class ItemOutAdapter (
     private val context: Context,
-    private val images : List<ItemList>,
-    val listener : (ItemList) -> Unit
+    private val images : List<Item>,
+    val listener : (Item) -> Unit
 ): RecyclerView.Adapter<ItemOutAdapter.ItemOutViewHolder>() {
     inner class ItemOutViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val icon: ImageView = view.findViewById<ImageView>(R.id.icon_list)
         val name: TextView = view.findViewById<TextView>(R.id.title_text_view)
         val desc: TextView = view.findViewById<TextView>(R.id.detail_text_view)
         val outBtn: Button = view.findViewById(R.id.button)
-        fun bindmodel(item: ItemList) {
-            icon.setImageResource(item.icons!!)
-            name.text = item.title
-            desc.text = item.detail
-            outBtn.setOnClickListener {
-                listener(item)
-            }
+
+        fun bindmodel(item: Item) {
+
+            val options: BitmapFactory.Options? = BitmapFactory.Options()
+            val bitmap = BitmapFactory.decodeByteArray(item.image, 0, item.image!!.size, options)
+
+            icon.setImageBitmap(bitmap)
+            name.text = item.id
+            desc.text = item.name
         }
     }
 
@@ -39,6 +45,29 @@ class ItemOutAdapter (
 
     override fun onBindViewHolder(holder: ItemOutViewHolder, position: Int) {
         holder.bindmodel(images[position])
+
+        holder.outBtn.setOnClickListener {
+
+            //Get position on selected item
+            val model = images[position]
+
+            val category = model.category
+            val name = model.name
+            val size = model.size
+            val qty = model.quantity
+
+            //Create intent with kotlin
+            val intent = Intent(context, stockOutForm::class.java)
+
+            // Now put all these items with put extra intent
+            intent.putExtra("ProductCategory", category)
+            intent.putExtra("ProductName", name)
+            intent.putExtra("ProductSize", size)
+            intent.putExtra("ProductQty", qty)
+
+            //Start another activity
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
