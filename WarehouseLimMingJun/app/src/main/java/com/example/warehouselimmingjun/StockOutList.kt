93ad6cd59b87b1b6d518951cbeabccb78cfbab11
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2020-12-01 18:35:26 +0800

    Update StockOutList.kt
    Remove redundant comments

--- WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/StockOutList.kt
+++ WarehouseLimMingJun/app/src/main/java/com/example/warehouselimmingjun/StockOutList.kt
@@ -45,26 +45,16 @@ class StockOutList : AppCompatActivity() {
             shirtList = dbHelper.retrieveShirtItem()
             recyclerView.layoutManager = LinearLayoutManager(this)
             recyclerView.adapter = ItemOutAdapter(this, shirtList, sessionId, sessionId1, shirt){
-                //val intent = Intent(this, stockOutForm::class.java)
+
                 intent.putExtra("Shirt", "shirt")
-                //val sessionId = intent.getStringExtra("emailAddress")
-                //val sessionId1 = intent.getStringExtra("name")
-                //intent.putExtra("emailAddress", sessionId)
-                //intent.putExtra("name", sessionId1)
-                //startActivity(intent)
             }
         }
         else if(shoes == "shoes"){
             shoeList =  dbHelper.retrieveShoesItem()
             recyclerView.layoutManager = LinearLayoutManager(this)
             recyclerView.adapter = ItemOutAdapter(this, shoeList, sessionId, sessionId1, shoes){
-                //val intent = Intent(this, stockOutForm::class.java)
+
                 intent.putExtra("Shoes", "shoes")
-                //val sessionId = intent.getStringExtra("emailAddress")
-                //val sessionId1 = intent.getStringExtra("name")
-                //intent.putExtra("emailAddress", sessionId)
-                //intent.putExtra("name", sessionId1)
-                //startActivity(intent)
             }
         }
     }
